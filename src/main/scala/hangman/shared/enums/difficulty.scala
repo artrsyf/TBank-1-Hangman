4b18f561a2663b2dd01260e83448bb0ebadb1204
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-08 21:36:04 +0300

    Make Difficulty enum open for all layers

---
+++ src/main/scala/hangman/shared/enums/difficulty.scala
@@ -0,0 +1,11 @@
+package hangman.shared.enums
+
+import scala.util.Random
+
+enum Difficulty:
+  case Easy, Medium, Hard
+
+object Difficulty:
+  def random: Difficulty = 
+    val values = Difficulty.values
+    values(Random.nextInt(values.length))
