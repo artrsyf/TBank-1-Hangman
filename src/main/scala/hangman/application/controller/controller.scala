fd0aa67bb8aee1a9d403cf12a87c60ef4060bd2d
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-09 17:55:59 +0300

    Create controller realization for user interaction

---
+++ src/main/scala/hangman/application/controller/controller.scala
@@ -0,0 +1,48 @@
+package hangman.application.controller
+
+import hangman.application.session.Session
+import hangman.application.session.default_session.DefaultSession
+import hangman.domain.dto.{CategoryDto, WordDto}
+
+case class GameController(val userSession: Session):
+    def processUserInput(difficultyNumber: Int, choosenCategory: String): GameController = 
+        val initializedSession = userSession.defineGameParams(difficultyNumber, choosenCategory)
+
+        copy(userSession = initializedSession)
+
+    def getGuessString: String = 
+        val currentState = userSession.getCurrentGameState
+
+        val adt = currentState.getAdt
+        val answer = currentState.getAnswer
+
+        answer.map {
+            case ch if adt.contains(ch.toLower) => ch
+            case _ => '_'
+        }.mkString
+
+    def guess(userInput: Char): GameController = 
+        val updatedGameSession = userSession.guess(userInput)
+
+        copy(userSession = updatedGameSession)
+
+    // def getRandomCategory: CategoryDto = CategoryRepo.getRandomCategory.getOrElse(
+    //     throw new NoSuchElementException("pending Exception")
+    // )
+
+    // def getParticularCategory(categoryName: String): List[WordDto] = 
+    //     val choosenCategory = CategoryRepo.getCategoryByName(categoryName).getOrElse(
+    //         throw new NoSuchElementException("pending Exception")
+    //     )
+
+    //     choosenCategory.words
+
+    def getOpenCategories: List[String] = 
+        val gameState = userSession.getCurrentGameState
+
+        val categories = gameState.getCategoryRepo.getAllCategories.getOrElse(
+                throw new NoSuchElementException("pending Exception")
+            )
+
+        categories.map(category => category.name)
+
