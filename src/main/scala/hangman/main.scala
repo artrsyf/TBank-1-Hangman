67529717586be2d95c2cdf9d5284c097075efd67
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-10 15:39:34 +0300

    Refactor entrypoint

--- src/main/scala/hangman/main.scala
+++ src/main/scala/hangman/main.scala
@@ -6,25 +6,20 @@ import hangman.application.session.default_session.DefaultSession
 import hangman.application.controller.GameController
 import hangman.application.game_state.default_game_state.DefaultSessionGameState
 
-@main def hello() = {
+@main def hello() =
   val repo = CategoryMemoryRepository()
   val gameState = DefaultSessionGameState(repo)
   val userSession = DefaultSession(1, gameState)
   val gameController = GameController(userSession)
   val gameConsole = ConsoleDisplay(gameController)
 
-  // Инициализация параметров игры
   val initedGameConsole = gameConsole.readAndUpdateUserParams
 
-  // Запуск игрового цикла
   gameLoop(initedGameConsole)
-}
 
-def gameLoop(consoleDisplay: ConsoleDisplay): Unit = {
-  // Рендерим текущее состояние игры
+def gameLoop(consoleDisplay: ConsoleDisplay): Unit =
   val updatedGameConsole = consoleDisplay.renderGame
 
   if !updatedGameConsole.gameController.userSession.getCurrentGameState.isGameEnded
   then gameLoop(updatedGameConsole)
   else updatedGameConsole.renderEnding
-}
